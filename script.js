982b5137bfa672a9e832c03c75a2ca0c8bedb37c
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-13 08:15:47 -0600

    passing premitive and object into function

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 const bookings = [];
 
-const createBook = function (flightNum, numPassenger = 1, price = 199) {
+const createBook = function (
+  flightNum,
+  numPassenger = 1,
+  price = 100 * numPassenger
+) {
   //ES5
   //   numPassenger = numPassenger || 1;
   //   price = price || 199;
@@ -16,5 +20,29 @@ const createBook = function (flightNum, numPassenger = 1, price = 199) {
   console.log(booking);
 };
 
-console.log(createBook('H123'));
-console.log(createBook('LH123', 2, 300));
+// console.log(createBook('H123'));
+// console.log(createBook('LH123', 2, 300));
+// console.log(createBook('LH234', 4));
+
+const flight = 'LH234';
+
+const ahmed = {
+  name: 'Ahmed Hussein',
+  passportNum: 12435332542,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passportNum === 12435332542) {
+    alert('Check In');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, ahmed);
+
+console.log(flight);
+console.log(ahmed);

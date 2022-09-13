5b3c830309d90120db6b2ea5ea3f7be7dde6fe8a
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-12 19:51:40 -0600

    default values for parameters in a function

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict';
 
-const booking = [];
+const bookings = [];
 
-const createBook = function (flightNum, numPassenger, price) {
+const createBook = function (flightNum, numPassenger = 1, price = 199) {
+  //ES5
+  //   numPassenger = numPassenger || 1;
+  //   price = price || 199;
+
   const booking = {
     flightNum,
     numPassenger,
     price,
   };
-  booking.push(booking);
+  bookings.push(booking);
+  console.log(booking);
 };
+
+console.log(createBook('H123'));
+console.log(createBook('LH123', 2, 300));
